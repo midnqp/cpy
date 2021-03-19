afaef97959f189deeb5b1336c270b834c618fa6e
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-03-19 13:23:14 +0200

    timsort

--- libavoidrepetition.h
+++ libavoidrepetition.h
@@ -165,29 +165,120 @@ double min(double numbers[], int arrlen) {
 }
 
 
-void swap(int *xp, int *yp) { 
-    int temp = *xp; 
-    *xp = *yp; 
-    *yp = temp; 
-} 
-
-  
-void selectionSort(int arr[], int n) { 
-    int i, j, min_idx; 
-  
-    // One by one move boundary of unsorted subarray 
-    for (i = 0; i < n-1; i++) 
-    { 
-        // Find the minimum element in unsorted array 
-        min_idx = i; 
-        for (j = i+1; j < n; j++) 
-          if (arr[j] < arr[min_idx]) 
-            min_idx = j; 
-  
-        // Swap the found minimum element with the first element 
-        swap(&arr[min_idx], &arr[i]); 
-    } 
+// This function sorts array from left index to
+// to right index which is of size atmost RUN
+void insertionSort(int arr[], int left, int right)
+{
+    for (int i = left + 1; i <= right; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= left && arr[j] > temp)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
 }
+ 
+// Merge function merges the sorted runs
+void merge(int arr[], int l, int m, int r)
+{
+     
+    // Original array is broken in two parts
+    // left and right array
+    int len1 = m - l + 1, len2 = r - m;
+    int left[len1], right[len2];
+    for (int i = 0; i < len1; i++)
+        left[i] = arr[l + i];
+    for (int i = 0; i < len2; i++)
+        right[i] = arr[m + 1 + i];
+ 
+    int i = 0;
+    int j = 0;
+    int k = l;
+ 
+    // After comparing, we 
+    // merge those two array
+    // in larger sub array
+    while (i < len1 && j < len2)
+    {
+        if (left[i] <= right[j])
+        {
+            arr[k] = left[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy remaining elements of left, if any
+    while (i < len1)
+    {
+        arr[k] = left[i];
+        k++;
+        i++;
+    }
+ 
+    // Copy remaining element of right, if any
+    while (j < len2)
+    {
+        arr[k] = right[j];
+        k++;
+        j++;
+    }
+}
+ 
+// Iterative Timsort function to sort the
+// array[0...n-1] (similar to merge sort)
+void timSort(int arr[], int n)
+{
+     
+    // Sort individual subarrays of size RUN
+    for (int i = 0; i < n; i+=RUN)
+        insertionSort(arr, i, min((i+RUN-1), 
+                                    (n-1)));
+ 
+    // Start merging from size RUN (or 32). 
+    // It will merge
+    // to form size 64, then 128, 256 
+    // and so on ....
+    for (int size = RUN; size < n; 
+                             size = 2*size)
+    {
+         
+        // pick starting point of 
+        // left sub array. We
+        // are going to merge 
+        // arr[left..left+size-1]
+        // and arr[left+size, left+2*size-1]
+        // After every merge, we 
+        // increase left by 2*size
+        for (int left = 0; left < n; 
+                             left += 2*size)
+        {
+             
+            // find ending point of 
+            // left sub array
+            // mid+1 is starting point 
+            // of right sub array
+            int mid = left + size - 1;
+            int right = min((left + 2*size - 1), 
+                                            (n-1));
+ 
+            // merge sub array arr[left.....mid] &
+            // arr[mid+1....right]
+              if(mid < right)
+                merge(arr, left, mid, right);
+        }
+    }
+}
+
 
 
 void printArray(int arr[], int size) { 
@@ -200,7 +291,7 @@ void printArray(int arr[], int size) {
 
 
 int* sorted(int *arr, int size){
-    selectionSort(arr, size);
+    timSort(arr, size);
 
     return arr;
 }
