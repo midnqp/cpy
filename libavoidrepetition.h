a158d3f9c98d2cd432874592aa05830522f67a97
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-03-19 07:50:16 +0200

    sorted function
    It returns a sorted array although, time complexity for this is n^2 so I'm pretty sure you won't merge this since there are other, faster sorting methods(I just couldn't get them working properly).

--- libavoidrepetition.h
+++ libavoidrepetition.h
@@ -158,3 +158,44 @@ double min(double numbers[], int arrlen) {
 	}
 	return minitem;
 }
+
+
+void swap(int *xp, int *yp) { 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+
+  
+void selectionSort(int arr[], int n) { 
+    int i, j, min_idx; 
+  
+    // One by one move boundary of unsorted subarray 
+    for (i = 0; i < n-1; i++) 
+    { 
+        // Find the minimum element in unsorted array 
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+  
+        // Swap the found minimum element with the first element 
+        swap(&arr[min_idx], &arr[i]); 
+    } 
+}
+
+
+void printArray(int arr[], int size) { 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", arr[i]); 
+    printf("\n"); 
+}
+
+
+
+int* sorted(int *arr, int size){
+    selectionSort(arr, size);
+
+    return arr;
+}
