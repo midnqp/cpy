dd5f43eb26f52beadcc6dfdb0e84d74a5e2e940c
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-03-19 16:08:52 +0200

    Deleted one function on accident

--- libavoidrepetition.h
+++ libavoidrepetition.h
@@ -177,6 +177,24 @@ int str_index(char *str, char item){
 }
 
 
+// This function sorts array from left index to
+// to right index which is of size atmost RUN
+void insertionSort(int arr[], int left, int right)
+{
+    for (int i = left + 1; i <= right; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= left && arr[j] > temp)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+}
+
+
 // Merge function merges the sorted runs
 void merge(int arr[], int l, int m, int r)
 {
